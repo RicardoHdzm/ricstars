d8de69d503dc87dcc805b9dea03145859f2c061d
Ricardo <jrhm95@gmail.com>
2026-07-17 22:20:07 -0700

    Agregar: The O.C. (Temporada 1)

--- js/data.js
+++ js/data.js
@@ -22,5 +22,15 @@ const entries = [
     resena: "Es un libro muy lindo. Me encantaron los protagonistas, la ambientacion y las frases.\n\n\"No recuerdo ninguna frase\"",
     puntuacion: 5,
     fecha: "2024-07-01"
+  },
+  {
+    id: 2,
+    titulo: "The O.C. (Temporada 1)",
+    categoria: "serie",
+    imagen: "assets/img/the-o-c-temporada-1-2.jpg",
+    generos: ["Drama"],
+    resena: "Es como la cuarta vez que la veo y apenas noto que Seth es una basura.",
+    puntuacion: 5,
+    fecha: "2026-04-01"
   }
 ];
